fab8715728d67bfea738d78b8035088208251296
fmkarakus <fatihkarakus@hotmail.com>
2022-10-26 10:15:29 +0200

    error logging implemented

--- src/main/java/com/switchfully/duckbusters/digibooky/api/DigibookyExceptionHandler.java
+++ src/main/java/com/switchfully/duckbusters/digibooky/api/DigibookyExceptionHandler.java
@@ -1,8 +1,11 @@
 package com.switchfully.duckbusters.digibooky.api;
 
+import com.switchfully.duckbusters.digibooky.service.ValidationService;
 import com.switchfully.duckbusters.digibooky.service.exceptions.UnauthorizatedException;
 import com.switchfully.duckbusters.digibooky.service.exceptions.UnknownPersonException;
 import com.switchfully.duckbusters.digibooky.service.exceptions.WrongPasswordException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
@@ -15,21 +18,26 @@ import static org.springframework.http.HttpStatus.*;
 
 @ControllerAdvice
 public class DigibookyExceptionHandler extends ResponseEntityExceptionHandler {
+    private final Logger logger = LoggerFactory.getLogger(DigibookyExceptionHandler.class);
     @ExceptionHandler(IllegalArgumentException.class)
     protected void courseID(IllegalArgumentException ex, HttpServletResponse response) throws IOException {
+        logger.error(ex.getMessage());
         response.sendError(BAD_REQUEST.value(), ex.getMessage());
     }
 
     @ExceptionHandler(UnauthorizatedException.class)
     protected void courseID(UnauthorizatedException ex, HttpServletResponse response) throws IOException {
+        logger.error(ex.getMessage());
         response.sendError(FORBIDDEN.value(), ex.getMessage());
     }
     @ExceptionHandler(UnknownPersonException.class)
     protected void courseID(UnknownPersonException ex, HttpServletResponse response) throws IOException {
+        logger.error(ex.getMessage());
         response.sendError(NOT_FOUND.value(), ex.getMessage());
     }
     @ExceptionHandler(WrongPasswordException.class)
     protected void courseID(WrongPasswordException ex, HttpServletResponse response) throws IOException {
+        logger.error(ex.getMessage());
         response.sendError(UNAUTHORIZED.value(), ex.getMessage());
     }
 
